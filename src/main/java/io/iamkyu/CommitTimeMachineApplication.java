f33a8d6629c08a4cd11c582cfd5a1e6358e8182e
kjnam <kyujnam@gmail.com>
2017-07-23 22:30:18 +0900

    init

---
+++ src/main/java/io/iamkyu/CommitTimeMachineApplication.java
@@ -0,0 +1,12 @@
+package io.iamkyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitTimeMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommitTimeMachineApplication.class, args);
+    }
+}
